47e7bef37099a0b31fb7229cdae224a3bd864121
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2025-04-12 03:34:51 -0300

    chore: arrays and slices

---
+++ 01-go-fundamentals/04-arrays-and-slices/sum.go
@@ -0,0 +1,22 @@
+package main
+
+func Sum(numbers []int) int {
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+	return total
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
